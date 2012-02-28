c4fc378e35d912a6f55ee3a8c844449860d09d04
Collin Schaafsma <collin@quickleft.com>
2012-02-28 14:54:37 -0700

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loyd::Application.config.secret_token = 'ff7cd8b528526cd91827b3edf5d6558865f31637e4fce980c4775fee4b525ec5841e4bdff5f701d62193e48d132191a60832e0e0947bb8f59c1d9bdae435775e'
